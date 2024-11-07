1491a4745304a566e5a68f98dc7ec39e95d5f2ff
FathyMuhamed <fathimuhamed10522@gmail.com>
2024-11-08 00:32:45 +0200

    welcome sauti quran app

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: ["i1.sndcdn.com"],
+  },
+};
+
+export default nextConfig;
